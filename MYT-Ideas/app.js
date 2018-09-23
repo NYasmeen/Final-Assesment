d02c31195900a0fb197ed9e872c0797795ef8185
NYasmeen <nazma.yasmeen@touch2success.com>
2018-09-23 23:18:17 +0530

    register and mongo

--- MYT-Ideas/app.js
+++ MYT-Ideas/app.js
@@ -3,17 +3,21 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+const fetch = require('node-fetch');
+
+
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+
 //Mongo
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/user";
 
 
-var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/');  // need to add the path 
+
 
 
 // view engine setup
@@ -28,6 +32,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- MYT-Ideas/routes/index.js
+++ MYT-Ideas/routes/index.js
@@ -1,15 +1,89 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = "mongodb://localhost:27017/user";
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next ) => {
+  MongoClient.connect(url, function(err, db) {
+    if (err) throw err;
+    var dbo = db.db("user");
+    /*Return only the documents with the address "Park Lane 38":*/
+        dbo.collection("ideas").find().toArray(function(err, result) {
+      if (err) throw err;
+     length = result.length;
 
-  var info = [{name : 'Nazma Yasmeen',idea : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "},
-  {name : "Ritu Mishra",idea : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "},
-  {name : "Simran Mittal",idea : "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. "}]
+  res.render('index', {l:length , Info : result, title: 'MYT-Ideas' , myturl : 'https://is2-ssl.mzstatic.com/image/thumb/Purple118/v4/d4/fe/cc/d4fecc10-12fe-7cea-7d53-8db73380f5e8/AppIcon_-1x_U007emarketing-85-220-0-9.png/246x0w.jpg'});
 
-  length = info.length;
-  res.render('index', {l:length , Info : info, title: 'MYT-Ideas' , myturl : 'https://is2-ssl.mzstatic.com/image/thumb/Purple118/v4/d4/fe/cc/d4fecc10-12fe-7cea-7d53-8db73380f5e8/AppIcon_-1x_U007emarketing-85-220-0-9.png/246x0w.jpg'});
-});
+      db.close();
+    });
+  });
+  })
+
+  router.post('/', (req, res) => {
+    MongoClient.connect(url, function(err, db) {
+      let { name, email, password } = req.body;
+      if (err) throw err;
+      var dbo = db.db("user");
+      var myobj = req.body;
+      
+      dbo.collection('collection').findOne({ email }, (err, result) => {
+        if (err) throw err;
+  if(result){
+    console.log("*Username is already exists.")
+  }else{
+  
+      dbo.collection("collection").insertOne(myobj, function(err, res) {
+        if (err) throw err;
+        console.log("1 document inserted");
+        db.close();
+      });
+    }
+    });
+      
+    });
+    })
+
+
+  
+
+  router.post('/like', (req, res) => {
+    
+    MongoClient.connect(url, function(err, db) {
+      if (err) throw err;
+      var dbo = db.db("user");
+      console.log(req.body);
+      var id = req.body.id;
+console.log("Hola" + id );
+      var myquery = { name: "yasmeen" };
+      var up = 19;
+      var newvalues = { $set: {like: up } };
+      dbo.collection("ideas").updateOne(myquery, newvalues, function(err, res) {
+        if (err) throw err;
+        console.log("1 document updated");
+        db.close();
+        
+
+      });
+    });
+    res.redirect('/')
+  });
+  
+
+  router.post('/clicked', (req, res) => {
+    
+    const click = {clickTime: new Date()};
+    console.log(click);
+    console.log(db);
+  
+    db.collection('clicks').save(click, (err, result) => {
+      if (err) {
+        return console.log(err);
+      }
+      console.log('click added to db');
+      res.sendStatus(201);
+    });
+  });
+
+
 
 module.exports = router;
